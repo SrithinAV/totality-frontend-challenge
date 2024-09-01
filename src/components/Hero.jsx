dc0fdbef8dd7a4261fd856cf06555c1edf47f562
Srithin A V <srithin@Srithins-MacBook-Air.local>
2024-09-01 18:21:28 +0530

    removed unwanted assets

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,33 +37,6 @@ const Hero = () => {
     
       },[])
 
-      useGSAP(()=>
-  {
-    const boxes = gsap.utils.toArray(scrollRef.current.children);
-
-    boxes.forEach((box) => {
-      
-      gsap.to(box,{
-
-        x:150 * (boxes.indexOf(box)+3),
-        y:150,
-        rotation:360,
-        opacity:0.6,
-        borderRadius:'100%',
-        scale:1.5,
-        scrollTrigger:
-        {
-          trigger:box,
-          start: 'bottom bottom',
-          end: 'top 20%',
-          scrub:true,
-          
-        },
-        ease:'power1.inOut'
-      })
-    
-    })
-  },{scope:scrollRef});
 
   return (
     <div className='mt-10 relative'>
@@ -74,10 +47,7 @@ const Hero = () => {
                 <h1 id='text' className='text-[40px] font-bold'><span className='text-purple-700'>Find</span> Your Dream Home By Us</h1>
                 <p className='para font-bold text-[18px] mt-4'> "Where your dreams meet the perfect home, we’re here to guide you every <br />Let us help you find the place you’ll love calling home"</p>
                 <div className="mt-20 absolute" ref={scrollRef}>
-        <div
-          id="scroll-pink"
-          className="opacity-0 text-[50px] flex justify-center items-center scroll-box w-20 h-20 rounded-full bg-Primary"
-        >🙂</div>
+       
        
       </div>
                <a href='#properties'><button className='mt-6 bg-Primary text-white px-6 py-3 rounded-md'>See Properties</button> </a> 
